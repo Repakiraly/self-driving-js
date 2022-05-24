af043bf84d7610cbb1cee95ab35b720a8dd5bb9e
Repakiraly <reepa94@gmail.com>
2022-05-24 12:26:28 +0200

    Simulation Finished: traffic, collision, sensors and road
    Code is finished for the simulation framework. Traffic has been added as well as collision and sensors

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car{
-    constructor(x,y,width,height){
+    constructor(x,y,width,height,controlType,maxSpeed=3){
         this.x=x;
         this.y=y;
         this.width=width;
@@ -7,32 +7,86 @@ class Car{
 
         this.speed=0;
         this.acceleration=0.2;
-        this.maxSpeed=5;
+        this.maxSpeed=maxSpeed;
         this.friction=0.05;
         this.angle=0;
         this.wheelSensitivity=0.01;
+        this.damaged=false;
 
-        this.controls = new Controls();
+        if(controlType!="DUMMY"){
+            this.sensor=new Sensor(this);
+        }
+        
+        this.controls=new Controls(controlType);
     }
 
-    update(){
+    update(roadBorders,traffic){
+        if(!this.damaged){
         this.#move();
+        this.polygon=this.#createPolygon();
+        this.damaged=this.#assessDamage(roadBorders,traffic);
+        }
+
+        if(this.sensor){
+            this.sensor.update(roadBorders,traffic);
+        }
         
+        
+    }
+
+    #assessDamage(roadBorders,traffic){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+
+        for(let i=0;i<traffic.length;i++){
+            if(polysIntersect(this.polygon,traffic[i].polygon)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    #createPolygon(){
+        const points=[];
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha = Math.atan2(this.width,this.height);
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad,
+        });
+
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad,
+        });
+
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad,
+        });
+
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad,
+        });
+
+        return points;
     }
 
     #move(){
         if(this.controls.forward){
             this.speed+=this.acceleration;
         }
-
-        if(this.controls.backward){
+        if(this.controls.reverse){
             this.speed-=this.acceleration;
         }
 
-        if(this.speed>this.maxSpeed) {
+        if(this.speed>this.maxSpeed){
             this.speed=this.maxSpeed;
         }
-
         if(this.speed<-this.maxSpeed/2){
             this.speed=-this.maxSpeed/2;
         }
@@ -40,11 +94,9 @@ class Car{
         if(this.speed>0){
             this.speed-=this.friction;
         }
-
         if(this.speed<0){
             this.speed+=this.friction;
         }
-
         if(Math.abs(this.speed)<this.friction){
             this.speed=0;
         }
@@ -54,35 +106,31 @@ class Car{
             if(this.controls.left){
                 this.angle+=this.wheelSensitivity*flip;
             }
-
             if(this.controls.right){
                 this.angle-=this.wheelSensitivity*flip;
             }
-
         }
 
-        
         this.x-=Math.sin(this.angle)*this.speed;
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx){
-    ctx.save();
-    ctx.translate(this.x,this.y);
-    ctx.rotate(-this.angle);
-
-    ctx.beginPath();
-    ctx.rect(
-        -this.width/2,
-        -this.height/2,
-        this.width,
-        this.height
-    );
-    ctx.fill();
-
-    ctx.restore();
+    draw(ctx,color){
+        if(this.damaged){
+            ctx.fillStyle="gray";
+        }else{
+            ctx.fillStyle=color;
+        }
+        ctx.beginPath();
+        ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
+        for(let i=1;i<this.polygon.length;i++){
+            ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
+        }
+        ctx.fill();
+            
+        if(this.sensor){
+            this.sensor.draw(ctx);
+        }
+        
     }
-
-
 }
-
